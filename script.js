0dc0a259d314f98393b165f5fc3598c52274e2ec
MANOJ1110E <emanoj453@gmail.com>
2025-08-19 12:34:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+// let a = "Manoj"
+// for(let i=0;i<a.length;i++){
+//     console.log(a[i])
+// }
+
+// let arr=[1,2,3,4,5,5]
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// let str1="sanjay"                          
+// let str2="nithish"
+// let str3="kanish"
+// console.log(typeof str1)                           
+// console.log(typeof str2)
+// console.log(typeof str3)
+// console.log(str1.length)
+// console.log(str2.length)
+// console.log(str3.length)
+// let str="   Manoj        "                  
+// console.log(str) 
+// console.log(str.trimStart())
+// console.log(str.trimEnd())
+//console.log(str.trim())
+
+// let str ="bathmavasan is a cute boy"
+// console.log(str.replace("cute","good"))
+
+// let str ="bathmavasan is a cute boy. bathmavasan is a handsome boy"
+//         console.log(str.replaceAll("bathmavasan","Manoj"))
+
+// let str="Manoj"
+// console.log(str[0])
+// console.log(str[4])
+
+//  let str ="Asswin is a cute boy. Asswin is a handsome boy"
+//     console.log(str.slice(0,9))
+
+//     let str ="Asswin is a cute boy. Asswin is a handsome boy"
+//     console.log(str.indexOf("s"))
+
+//  function abc(){
+//         for(let i=0;i<1000;i++){
+//         document.writeln("Sorry")
+//         }
+//         }
+//         abc()
+
+
+// let fun = function () {
+//         console.log("HI,I am Manoj")
+// }
+// fun()
+
+
+//   let fun= ()=>{
+//         console.log("Hi i am Manoj")
+//         } 
+//         fun()
+
+// (function() => {
+//        console.log("manoj")
+// }())
+
+// (()=>{
+//     console.log("Manoj")
+// })()
+
+// let arr=[1,"Nikhil",["HTML","CSS","JS"],false,()=>{}]    
+//         console.log(arr.length)
+
+// let arr=["Bathamavasn","lavanya","ezhil","Manoj","Jeevann"]
+//         console.log(arr.slice(2,4))
+
+    let obj={
+        name:"Manoj",
+        age:21,
+        
+        }
+   
+        console.log(obj.name)
+        console.log(obj["age"])
